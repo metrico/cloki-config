6e538a499062ea75869dd8e8c82cd4c21978c94f
akvlad <akvlad90@gmail.com>
2022-04-23 13:37:22 +0300

    kafka config

--- config/writer/writer.go
+++ config/writer/writer.go
@@ -17,6 +17,29 @@ type MQTTTopicConf struct {
 	TimeUnit string `json:"time_unit" mapstructure:"time_unit" default:""`
 }
 
+type KafkaTopicConf struct {
+	Name           string            `json:"name" mapstructure:"name" default:""`
+	Tags           map[string]string `json:"tags" mapstructure:"tags" default:""`
+	IncludeTopic   bool              `json:"include_topic" mapstructure:"include_topic" default:"true"`
+	IncludeHeaders bool              `json:"include_headers" mapstructure:"include_headers" default:"true"`
+	IncludeKey     bool              `json:"include_key" mapstructure:"include_key" default:"true"`
+	// json or empty
+	Format    string     `json:"format" mapstructure:"format" default:""`
+	Extract   [][]string `json:"extract" mapstructure:"extract" default:"[]"`
+	Partition int        `json:"partition" mapstructure:"partition" default:"0"`
+}
+
+type KafkaConf struct {
+	Host string `json:"host" mapstructure:"host" default:""`
+	Port int    `json:"port" mapstructure:"port" default:"0"`
+	// SASL auth type:  Plain/Scram
+	AuthType string         `json:"auth_type" mapstructure:"auth_type" default:""`
+	Username string         `json:"username" mapstructure:"username" default:""`
+	Password string         `json:"password" mapstructure:"password" default:""`
+	Topic    KafkaTopicConf `json:"topic" mapstructure:"topic" default:""`
+	GroupID  string         `json:"consumer_group" mapstructure:"consumer_group" default:""`
+}
+
 //
 type ClokiWriterSettingServer struct {
 	MQTT_CLIENT struct {
@@ -27,4 +50,5 @@ type ClokiWriterSettingServer struct {
 		Password   string        `json:"password" mapstructure:"password" default:""`
 		Topic      MQTTTopicConf `json:"topic" mapstructure:"topic" default:""`
 	} `json:"mqtt" mapstructure:"mqtt"`
+	KAFKA_CLIENT KafkaConf `json:"kafka" mapstructure:"kafka" default:""`
 }
