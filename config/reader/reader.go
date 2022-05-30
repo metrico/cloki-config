71255e14dc820abdb88a1e022e31ff21180a7303
akvlad <akvlad90@gmail.com>
2022-05-30 14:38:42 +0000

    cloki reader as proxy settings

--- config/reader/reader.go
+++ config/reader/reader.go
@@ -2,4 +2,6 @@ package reader
 
 // ============================ READER ONLY ================================= //
 type ClokiReaderSettingServer struct {
+	ViewPath    string `json:"view_path" mapstructure:"view_path" default:""`
+	WriterProxy string `json:"writer_proxy" mapstructure:"writer_proxy" default:""`
 }
