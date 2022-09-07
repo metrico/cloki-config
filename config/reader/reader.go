bd6fffebb1eafd074a1d9df2c37f07eab746d9e6
akvlad <akvlad90@gmail.com>
2022-09-07 12:26:58 +0000

    default view path

--- config/reader/reader.go
+++ config/reader/reader.go
@@ -2,6 +2,6 @@ package reader
 
 // ============================ READER ONLY ================================= //
 type ClokiReaderSettingServer struct {
-	ViewPath    string `json:"view_path" mapstructure:"view_path" default:""`
+	ViewPath    string `json:"view_path" mapstructure:"view_path" default:"/etc/qryn-view"`
 	WriterProxy string `json:"writer_proxy" mapstructure:"writer_proxy" default:""`
 }
