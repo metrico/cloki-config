3d831422596ea31dbd64c063a64c7e870f9de64c
akvlad <akvlad90@gmail.com>
2023-11-01 16:03:22 +0200

    concurrency setting

--- config/config.go
+++ config/config.go
@@ -124,6 +124,7 @@ type ClokiBaseSettingServer struct {
 		Gzip          bool   `json:"gzip" mapstructure:"gzip" default:"true"`
 		GzipStatic    bool   `json:"gzip_static" mapstructure:"gzip_static" default:"true"`
 		Debug         bool   `json:"debug" mapstructure:"debug" default:"false"`
+		Concurrency   int    `json:"concurrency" mapstructure:"concurrency" default:"350"`
 		Cors          struct {
 			Origin string `json:"origin" mapstructure:"origin" default:"*"`
 			Enable bool   `json:"enable" mapstructure:"enable" default:"false"`
