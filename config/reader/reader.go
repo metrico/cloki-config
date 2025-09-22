74363f25ec4e05095f6ddbcbc4ebc1087f7035d3
akvlad <akvlad90@gmail.com>
2025-09-22 17:42:26 +0300

    OmitEmptyValues config

--- config/reader/reader.go
+++ config/reader/reader.go
@@ -2,6 +2,7 @@ package reader
 
 // ============================ READER ONLY ================================= //
 type ClokiReaderSettingServer struct {
-	ViewPath    string `json:"view_path" mapstructure:"view_path" default:"/etc/qryn-view"`
-	WriterProxy string `json:"writer_proxy" mapstructure:"writer_proxy" default:""`
+	ViewPath        string `json:"view_path" mapstructure:"view_path" default:"/etc/qryn-view"`
+	WriterProxy     string `json:"writer_proxy" mapstructure:"writer_proxy" default:""`
+	OmitEmptyValues bool   `json:"omit_empty_values" mapstructure:"omit_empty_values" default:"false"`
 }
