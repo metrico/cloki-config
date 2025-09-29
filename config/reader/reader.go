f9a06df587cf98541dfdd4b8c7b01e0b8f23cc3e
akvlad <akvlad90@gmail.com>
2025-09-29 22:21:31 +0300

    Compatibility to v4.0.19 config

--- config/reader/reader.go
+++ config/reader/reader.go
@@ -5,4 +5,5 @@ type ClokiReaderSettingServer struct {
 	ViewPath        string `json:"view_path" mapstructure:"view_path" default:"/etc/qryn-view"`
 	WriterProxy     string `json:"writer_proxy" mapstructure:"writer_proxy" default:""`
 	OmitEmptyValues bool   `json:"omit_empty_values" mapstructure:"omit_empty_values" default:"false"`
+	Compat_4_0_19   bool   `json:"compat_4_0_19" mapstructure:"compat_4_0_19" default:"false"`
 }
